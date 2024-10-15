93bf05f86605961efc45c44ac67d4e38216c5e04
Krzysztof Swend <krzysiekswend@gmail.com>
2024-10-15 20:21:24 +0200

    added DatabaseConnection class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,6 @@
 package org.example;
 
 import org.example.elements.*;
-
-import java.sql.*;
 import java.time.LocalDate;
 
 
@@ -10,31 +8,6 @@ import java.time.LocalDate;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(
-                    "jdbc:mysql://127.0.0.1:3306/library_schema",
-                    "root",
-                    "haslo1234"
-            );
-
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM books");
-
-            while (resultSet.next()){
-                System.out.print(resultSet.getInt("idbooks") + ": ");
-                System.out.print(resultSet.getString("title") + " ");
-                System.out.println(resultSet.getInt("avaliableCopies"));
-
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-
-
-
 
         //Welcome
         Library library = new Library();
